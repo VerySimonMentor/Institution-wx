58e4eba0bbd9c104ecadf36cef6ffbc77dfd6abc
qjj <rhysqiuj@gmail.com>
2024-05-24 17:10:25 +0800

    wairi

--- pages/home/toolPages/moneyPolicy/moneyPolicy.js
+++ pages/home/toolPages/moneyPolicy/moneyPolicy.js
@@ -39,7 +39,7 @@ Page({
     const loginTocken = wx.getStorageSync('loginTocken');
     const that = this;
     wx.request({
-      url: app.globalData.domain + '/wx/getCountryDetail?loginTocken=' + loginTocken + '&searchContent=' + searchContent + '&countryListIndex=' + this.data.selectedCountryIndex + '&selectedProvinceMap' + JSON.stringify(this.data.selectedProvinceMap) + '&selectedSchoolTypeMap' + JSON.stringify(this.data.selectedSchoolTypeMap),
+      url: app.globalData.domain + '/wx/getCountryDetail?loginTocken=' + loginTocken + '&searchContent=' + searchContent + '&countryListIndex=' + this.data.selectedCountryIndex + '&selectedProvinceMap' + encodeURIComponent(JSON.stringify(this.data.selectedProvinceMap)) + '&selectedSchoolTypeMap' + encodeURIComponent(JSON.stringify(this.data.selectedSchoolTypeMap)),
       method: 'GET',
       success: function(res){
         that.setData({
