086bcccc2694a46128834db1cff16ea3cfaa1df1
dyf <verysimon@mentor7.cn>
2024-05-24 15:29:15 +0800

    develop show

--- pages/home/toolPages/moneyPolicy/moneyPolicy.js
+++ pages/home/toolPages/moneyPolicy/moneyPolicy.js
@@ -3,8 +3,11 @@ Page({
     countryList: [],
     provinceList: [],
     schoolTypeList: [],
+    schoolList: [],
     selectedCountryIndex: 0,
     showFilterMenu: false,
+    selectedProvinceMap: {},
+    selectedSchoolTypeMap: {},
   },
   onLoad: function (options) {
     const app = getApp();
@@ -19,6 +22,7 @@ Page({
         });
       }
     });
+    this.onConfirmSchoolName({detail: {value: ''}});
   },
   onTapCountry(e) {
     var countryListIndex = e.currentTarget.dataset.index;
@@ -27,11 +31,57 @@ Page({
     });
   },
   onConfirmSchoolName(e) {
-    const schoolName = e.detail.value;
+    const searchContent = e.detail.value;
+    const app = getApp();
+    const loginTocken = wx.getStorageSync('loginTocken');
+    const that = this;
+    wx.request({
+      url: app.globalData.domain + '/wx/getCountryDetail?loginTocken=' + loginTocken + '&searchContent=' + searchContent + '&countryListIndex=' + this.data.selectedCountryIndex + '&selectedProvinceMap' + JSON.parse(this.data.selectedProvinceMap) + '&selectedSchoolTypeMap' + JSON.parse(this.data.selectedSchoolTypeMap),
+      method: 'GET',
+      success: function(res){
+        that.setData({
+          schoolList: res.data.countryDetail,
+          schoolTypeList: res.data.schoolType,
+          provinceList: res.data.province,
+        });
+      }
+    });
   },
   onTapFilterMenu(e) {
     this.setData({
       showFilterMenu: !this.data.showFilterMenu,
     });
-  }
+  },
+  onTapCancel(e) {
+    this.setData({
+      showFilterMenu: false,
+    });
+  },
+  onTapConfirm(e) {
+    this.setData({
+      showFilterMenu: false,
+    });
+  },
+  onTapProvince(e) {
+    var provinceListIndex = e.currentTarget.dataset.index;
+    if (provinceListIndex in this.data.selectedProvinceMap) {
+      delete this.data.selectedProvinceMap[provinceListIndex];
+    } else {
+      this.data.selectedProvinceMap[provinceListIndex] = true;
+    }
+    this.setData({
+      selectedProvinceMap: this.data.selectedProvinceMap,
+    });
+  },
+  onTapSchoolType(e) {
+    var schoolTypeListIndex = e.currentTarget.dataset.index;
+    if (schoolTypeListIndex in this.data.selectedSchoolTypeMap) {
+      delete this.data.selectedSchoolTypeMap[schoolTypeListIndex];
+    } else {
+      this.data.selectedSchoolTypeMap[schoolTypeListIndex] = true;
+    }
+    this.setData({
+      selectedSchoolTypeMap: this.data.selectedSchoolTypeMap,
+    });
+  },
 })
